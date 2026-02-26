7eb798a4d266126bacf08e3f1cd58024f7df0c62
palrang22 <kseunghui@gmail.com>
2026-02-26 22:40:19 +0900

    [feat] add network layer with URLSession async/await

--- ChimHahaApp/Data/Network/APIClient.swift
+++ ChimHahaApp/Data/Network/APIClient.swift
@@ -5,38 +5,33 @@
 //  Created by 김승희 on 2/26/26.
 //
 
-import Combine
 import Foundation
 
 
 struct APIClient {
     private let session: URLSession
-    
+
     init(session: URLSession = .shared) {
         self.session = session
     }
-    
-    func fetch<T: Decodable>(_ endpoint: Endpoint) -> AnyPublisher<T, NetworkError> {
+
+    func fetch<T: Decodable>(_ endpoint: Endpoint) async throws -> T {
         guard let url = endpoint.url else {
-            return Fail(error: NetworkError.invalidURL)
-                .eraseToAnyPublisher()
+            throw NetworkError.invalidURL
         }
-        
-        return session.dataTaskPublisher(for: url)
-            .tryMap { data, response in
-                guard let http = response as? HTTPURLResponse,
-                      (200...299).contains(http.statusCode) else {
-                    let code = (response as? HTTPURLResponse)?.statusCode ?? -1
-                    throw NetworkError.requestFailed(statusCode: code)
-                }
-                return data
-            }
-            .decode(type: T.self, decoder: JSONDecoder())
-            .mapError { error -> NetworkError in
-                if let e = error as? NetworkError { return e }
-                if error is DecodingError { return .decodingFailed }
-                return .unknown(error.localizedDescription)
-            }
-            .eraseToAnyPublisher()
+
+        let (data, response) = try await session.data(from: url)
+
+        guard let http = response as? HTTPURLResponse,
+              (200...299).contains(http.statusCode) else {
+            let code = (response as? HTTPURLResponse)?.statusCode ?? -1
+            throw NetworkError.requestFailed(statusCode: code)
+        }
+
+        do {
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch {
+            throw NetworkError.decodingFailed
+        }
     }
 }
